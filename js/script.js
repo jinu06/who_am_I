3a4c1a2de8e0927b903751970b4d297d5d937abc
jinu <jinuwdr05@gmail.com>
2021-07-12 20:52:24 +0530

    skils design done

--- js/script.js
+++ js/script.js
@@ -93,3 +93,20 @@ function typeWriter(id, ar) {
     }
   }
 }
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("navigator");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
